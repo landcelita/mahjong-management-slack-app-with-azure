196a94b8e0259bf1d1f07feca08d270bf1bcd4c1
landcelita <nyoo.www@gmail.com>
2022-04-22 22:13:25 +0900

    update sql

--- sqlcrud.py
+++ sqlcrud.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Union
+from typing import Any, List, Tuple, Union
 import pyodbc
 from dotenv import load_dotenv
 
@@ -12,22 +12,27 @@ username = os.environ.get('SQL_USERNAME')
 password = '{' + os.environ.get('SQL_PASSWORD') + '}'
 driver= '{'+ os.environ.get('SQL_DRIVER') + '}'
 
+def enclose_quot(val):
+    if isinstance(val, str):
+        return "'" + val + "'"
+    else:
+        return str(val)
+
 # C
 def exec_insert_sql(table: str, vals: List[str], cols: Union[List[str], None] = None) -> None :
-    # table: テーブル名, vals: 挿入する値(List), cols: 列の名前(List)
+    # table: テーブル名, vals: 挿入する値, cols: 列の名前
     conn =  pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+\
         ';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cur = conn.cursor()
 
+    # 前処理
+    for i in range(len(vals)):
+        vals[i] = enclose_quot(vals[i])
+    
     sql = "INSERT INTO [dbo].[" + table + "]"
     if cols is not None:
         sql += "(" + ", ".join(cols) + ")"
     sql += "\n"
-    for i in range(len(vals)):
-        if isinstance(vals[i], str):
-            vals[i] = "'" + vals[i] + "'"
-        else:
-            vals[i] = str(vals[i])
     sql += " VALUES (" + ", ".join(vals) + ")"
 
     cur.execute(sql)
@@ -36,8 +41,8 @@ def exec_insert_sql(table: str, vals: List[str], cols: Union[List[str], None] = 
     conn.close()
 
 # R
-def exec_select_sql(table: str, cols: Union[List[str], None] = None, where: Union[str, None] = None):
-    # table: テーブル名, cols: 取得したい行(List), where: 指定する条件(str)
+def exec_select_sql(table: str, cols: Union[List[str], None] = None, where: Union[str, None] = None) -> List[Tuple]:
+    # table: テーブル名, cols: 取得したい行, where: 指定する条件
     conn =  pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+\
         ';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cur = conn.cursor()
@@ -57,7 +62,38 @@ def exec_select_sql(table: str, cols: Union[List[str], None] = None, where: Unio
     conn.close()
     return ret
 
-def exec_sql(sql):
+# U
+def exec_update_sql(table: str, cols: Union[List[str], str], vals: Union[List[Any], Any], where: Union[str, None]) -> None:
+    # table: テーブル名, cols: 更新したい列名, vals: 更新する値, where: 条件節
+    conn =  pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+\
+        ';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cur = conn.cursor()
+
+    # 前処理
+    if isinstance(vals, List):
+        for i in range(len(vals)):
+            vals[i] = enclose_quot(vals[i])
+    else:
+        vals = enclose_quot(vals)
+    
+    sql = "UPDATE [dbo].[" + table + "]\nSET "
+    if isinstance(cols, List):
+        sets = []
+        for i in range(len(cols)):
+            sets.append(cols[i] + ' = ' + vals[i])
+        sql += ", ".join(sets)
+    else:
+        sql += cols + ' = ' + vals
+    sql += "\n"
+    if where is not None:
+        sql += "WHERE " + where
+
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def exec_sql(sql: str) -> None:
     conn =  pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+\
         ';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cur = conn.cursor()
